c9363d2fb4136dcccfab54e086602f3611e4b30a
jiac5@uci.edu <jiac5@uci.edu>
2018-02-02 22:39:50 -0800

    Create a UI project with VTK support for visualization

---
+++ UI/GeometryProcessing/FormGeometryProcessing.Designer.cs
@@ -0,0 +1,130 @@
+﻿#define USE_VTK
+namespace GeometryProcessing {
+    partial class FormGeometryProcessing {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplacianSmoothToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.qualityStatisticsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.processToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1022, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.laplacianSmoothToolStripMenuItem,
+            this.qualityStatisticsToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // laplacianSmoothToolStripMenuItem
+            // 
+            this.laplacianSmoothToolStripMenuItem.Name = "laplacianSmoothToolStripMenuItem";
+            this.laplacianSmoothToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.laplacianSmoothToolStripMenuItem.Text = "Laplacian Smooth";
+            this.laplacianSmoothToolStripMenuItem.Click += new System.EventHandler(this.laplacianSmoothToolStripMenuItem_Click);
+            // 
+            // qualityStatisticsToolStripMenuItem
+            // 
+            this.qualityStatisticsToolStripMenuItem.Name = "qualityStatisticsToolStripMenuItem";
+            this.qualityStatisticsToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.qualityStatisticsToolStripMenuItem.Text = "Quality Statistics";
+            this.qualityStatisticsToolStripMenuItem.Click += new System.EventHandler(this.qualityStatisticsToolStripMenuItem_Click);
+#if USE_VTK
+            InitializeRenderWindow();
+#endif
+            // 
+            // FormGeometryProcessing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 811);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormGeometryProcessing";
+            this.Text = "GeometryProcessing";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+#endregion
+        private void InitializeRenderWindow() {
+            this.renderWindowControl = new Kitware.VTK.RenderWindowControl();
+            this.renderWindowControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.renderWindowControl.AddTestActors = false;
+            this.renderWindowControl.Location = new System.Drawing.Point(3, 3);
+            this.renderWindowControl.Name = "renderWindowControl1";
+            this.renderWindowControl.Size = new System.Drawing.Size(600, 400);
+            this.renderWindowControl.TabIndex = 0;
+            this.renderWindowControl.TestText = null;
+            this.Controls.Add(this.renderWindowControl);
+        }
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem laplacianSmoothToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem qualityStatisticsToolStripMenuItem;
+#if USE_VTK
+        private Kitware.VTK.RenderWindowControl renderWindowControl;
+#endif
+    }
+}
+
---
+++ UI/GeometryProcessing/FormGeometryProcessing.cs
@@ -0,0 +1,141 @@
+﻿#define USE_VTK
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Kitware.VTK;
+
+namespace GeometryProcessing {
+    public partial class FormGeometryProcessing : Form {
+        public FormGeometryProcessing() {
+            InitializeComponent();
+        }
+
+        g3.DMesh3 _mesh = null;
+
+        vtkActor actor = null;
+                
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK) {
+                var path = dialog.FileName;
+
+                var builder = new g3.DMesh3Builder();
+                var reader = new g3.StandardMeshReader() { MeshBuilder = builder };
+                g3.IOReadResult result = reader.Read(path, g3.ReadOptions.Defaults);
+                if (result.code == g3.IOCode.Ok) {
+                    var meshes = builder.Meshes;
+
+                    _mesh = meshes[0];
+
+                    DrawMesh();
+                }
+            }
+        }
+
+        private void qualityStatisticsToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (_mesh == null) {
+                
+            } else { 
+                var genus = g3.MeshMeasurements.Genus(_mesh).Genus;
+                MessageBox.Show("Genus:"+genus+"\n");
+            }
+        }
+
+        private void DrawMesh() {
+#if USE_VTK
+            var renderer = this.renderWindowControl.RenderWindow.GetRenderers().GetFirstRenderer();
+
+            if (this.actor != null) {
+                renderer.RemoveActor(this.actor);
+                this.actor.Dispose();
+                this.actor = null;
+            }
+
+            var points = vtkPoints.New();
+            points.SetNumberOfPoints(_mesh.VertexCount);
+            int index = 0;
+            foreach (var vertex in _mesh.Vertices()) {
+                points.SetPoint(index++, vertex.x, vertex.y, vertex.z);
+            }
+
+            var triangles = vtkCellArray.New();
+            triangles.Allocate(triangles.EstimateSize(_mesh.TriangleCount, 3), _mesh.TriangleCount / 2);
+
+            
+            foreach (var triangle in _mesh.Triangles()) {
+                vtkIdList verts = vtkIdList.New();
+                verts.InsertNextId(triangle.a);
+                verts.InsertNextId(triangle.b);
+                verts.InsertNextId(triangle.c);
+                triangles.InsertNextCell(verts);
+            }
+
+            var polyData = vtkPolyData.New();
+            polyData.SetPoints(points);
+            polyData.SetPolys(triangles);
+
+            points.Dispose();
+            triangles.Dispose();
+
+            var mapper = vtkPolyDataMapper.New();
+            mapper.SetInput(polyData);
+
+            actor = vtkActor.New();
+            actor.SetMapper(mapper);
+
+            renderer.AddActor(actor);
+
+            this.renderWindowControl.RenderWindow.Render();
+#endif
+        }
+        
+        private bool CheckAndWarnMeshExist() {
+            if (_mesh == null) {
+                MessageBox.Show("No mesh loaded yet.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void laplacianSmoothToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (!CheckAndWarnMeshExist()) {
+                return;
+            }
+
+            // JC: Note that this is not the g3.LaplacianMeshSmoother. I am not sure what that function does.
+            // According to MeshLab, Laplacian smoothing means: for each vertex, calculate the average position with nearest vertex.
+
+
+            // TODO(jiac5): a lot of optimization is needed here.
+            List<g3.Vector3d> temp = new List<g3.Vector3d>(_mesh.VertexCount);
+            for (int i = 0; i < _mesh.VertexCount; ++i) {
+                temp.Add(new g3.Vector3d());
+            }
+            foreach (var v in _mesh.VertexIndices()) {
+                var avg = new g3.Vector3d();
+                int count = 0;
+                foreach (var neighbor_v in _mesh.VtxVerticesItr(v)) {
+                    avg = avg + _mesh.GetVertex(neighbor_v);
+                    count++;
+                }
+                avg = avg / count;
+                temp[v] = avg;
+            }
+
+            foreach (var v in _mesh.VertexIndices()) {
+                _mesh.SetVertex(v, temp[v]);
+            }
+
+            DrawMesh();
+        }
+    }
+}
